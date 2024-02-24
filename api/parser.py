535c07e0922e71514cf757b1c0e56f26922e6bd2
Benny Iko <benny.iko.it@gmail.com>
2024-02-24 14:45:12 -0500

    On branch master: adds parser.py 1.0.0
    Initial commit
    Changes to be committed:
    	new file:   api/parser.py
    	adds get_all_player_names(filename)
    	adds get_away_player_names(filename, amt)
    	adds parse_espn_box(filename)

---
+++ api/parser.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+
+#this function returns home and away player names.
+def get_all_player_names(filename):
+    stats = []
+    with open(filename) as fp:
+        soup = BeautifulSoup(fp, "html.parser")
+    for tag in soup.find_all("a", class_="AnchorLink truncate db Boxscore__AthleteName"):
+        for el in tag.contents:
+            if not el.name == 'div' and not el.name == 'a' and not el.name == 'span':
+                stats.append(el)
+    return stats
+
+#this function works identical to get_all_player_names(filename), with caveat that only away player names are returned. "amt" refers to the number of players listed on away team roster
+def get_away_player_names(filename, amt):
+    stats = []
+    with open(filename) as fp:
+        soup = BeautifulSoup(fp, "html.parser")
+    for tag in soup.find_all("a", class_="AnchorLink truncate db Boxscore__AthleteName", limit = amt):
+        for el in tag.contents:
+            if not el.name == 'div' and not el.name == 'a' and not el.name == 'span':
+                stats.append(el)
+    return stats
+
+#this function utilizes Beautiful Soup API to parse an ESPN box score provided as HTML. It returns a list in CSV format. 
+def parse_espn_box(filename):
+    stats = []
+    with open(filename) as fp:
+        soup = BeautifulSoup(fp, "html.parser")
+    for tag in soup.find_all("td", class_="Table__TD"):
+        for el in tag.contents:
+            if not el.name == 'div' and not el.name == 'a' and not el.name == 'span':
+                stats.append(el)
+    return stats
