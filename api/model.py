65de6b9aca705d09589a3d3fbc0d0c86aa9dba79
Benny Iko <benny.iko.it@gmail.com>
2024-02-26 01:27:57 -0500

    On branch master: adds api/model.py 1.2.0 Changes to be committed: 	new file:   api/model.py 	adds player, overtime_values_periods, normal_time_values_periods dictionaries

---
+++ api/model.py
@@ -0,0 +1,49 @@
+
+player = {
+    "player_min": 1,
+    "player_fg": 2,
+    "player_3pt": 3,
+    "player_ft": 4,
+    "player_o_reb": 5, 
+    "player_d_reb": 6,
+    "player_reb": 7,
+    "player_ast": 8,
+    "player_stl": 9,
+    "player_blk": 10,
+    "player_to": 11,
+    "player_pf": 12,
+    "player_plus_minus": 13,
+    "player_pts": 14,
+}
+
+overtime_values_periods ={
+    "away": 0,
+    "a_first": 1,
+    "a_second": 2,
+    "a_third": 3,
+    "a_fourth": 4,
+    "a_ot": 5,
+    "a_total": 6,
+    "h": 7,
+    "h_first": 8,
+    "h_second": 9,
+    "h_third": 10,
+    "h_fourth": 11,
+    "h_ot": 12,
+    "h_total": 13,
+}
+
+normal_time_values_periods ={
+    "away": 0,
+    "a_first": 1,
+    "a_second": 2,
+    "a_third": 3,
+    "a_fourth": 4,
+    "a_total": 5,
+    "h": 6,
+    "h_first": 7,
+    "h_second": 8,
+    "h_third": 9,
+    "h_fourth": 10,
+    "h_total": 11,
+}
