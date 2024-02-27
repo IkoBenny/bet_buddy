4eba72ce22dd74189a4f1d2941b70e0511707ff1
Benny Iko <benny.iko.it@gmail.com>
2024-02-27 01:54:23 -0500

    On branch master: modifies parser.py 1.4.0 Changes to be committed: 	modified:   api/parser.py 	adds get_away_box_score(filename, stats) 	adds get_overtime_totals_by_quarter(box) 	adds get_player_dnp(box, counter) 	adds get_player_stats(box, counter)

--- api/parser.py
+++ api/parser.py
@@ -11,6 +11,36 @@ def get_all_player_names(filename):
                 stats.append(el)
     return stats
 
+#this function displays the box score for away team in CSV format.
+def get_away_box_score(filename, stats):
+    players = 0
+    roster = []
+    athlete = [] 
+    for x in range(13, len(stats), 14):
+        if x == 13:
+            athlete = get_player_stats(stats, x)
+            players = players + 1
+            roster.append(athlete)
+        else:
+            if not get_player_stats(stats, x) == "done":
+                athlete = get_player_stats(stats, x)
+                players = players + 1
+                roster.append(athlete)
+            else:
+                break
+    counter2 = 140
+    dnp =0
+    while not get_player_dnp(stats, counter2) == "done":
+        dnp = dnp + 1
+        athlete = get_player_dnp(stats, counter2)
+        roster.append(athlete)
+        counter2 = counter2 + 1
+        
+    names = get_away_player_names(filename, len(roster))    
+    for name, player in zip(names, roster):
+         roster.append(name)      
+    print(roster)
+
 #this function works identical to get_all_player_names(filename), with caveat that only away player names are returned. "amt" refers to the number of players listed on away team roster
 def get_away_player_names(filename, amt):
     stats = []
@@ -22,12 +52,36 @@ def get_away_player_names(filename, amt):
                 stats.append(el)
     return stats
 
+#this function returns the final values for all quarters including OT.
+def get_overtime_totals_by_quarter(box):
+    stats = []
+    for key, value in zip(overtime_values_periods.keys(), overtime_values_periods.values()):
+        stats.append(str(key) + ", " + str(box[value]))
+    return stats
+
 #this function determines if a game finished in regular time.
 def is_overtime_game(box):
     if box[6].isnumeric():
         return True
     else:
         return False
+
+#this helper-function is executed whenever get_away_box_score(filename, stats) is called. 
+def get_player_dnp(box, counter):
+    if box[counter] == "DNP-COACH'S DECISION":
+        return "DNP-COACH'S DECISION"
+    else:
+        return "done"
+
+#this function parses a box score row. It returns a list in CSV format. 
+def get_player_stats(box, counter):
+    stats = []
+    for key, value in zip(player.keys(), player.values()):
+        if not box[value + counter] == "DNP-COACH'S DECISION":
+            stats.append(key + ", " + str(box[value + counter]))
+        else:
+            return "done"
+    return stats
 
 #this function utilizes Beautiful Soup API to parse an ESPN box score provided as HTML. It returns a list in CSV format. 
 def parse_espn_box(filename):
