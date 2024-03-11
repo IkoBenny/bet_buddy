ff67f327dba0807e54e1669bded50848c3a5471c
Benny Iko <benny.iko.it@gmail.com>
2024-03-10 23:17:46 -0400

    On branch master: adds Soup wrapper soup.py 3.5.5 Changes to be committed: 	new file:   soup.py 	adds soup wrapper

---
+++ api/soup.py
@@ -0,0 +1,9 @@
+class soup:
+    def _init(filename):
+        self.soup_obj = get_soup(filename)
+    
+    #this function returns the Beautiful Soup object.
+    def get_soup(filename):
+        with open(filename) as fp:
+            soup = BeautifulSoup(fp, "html.parser")
+            return soup
