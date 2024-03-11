dcd14a202811bb87a4165c4fc2e8411cce70447d
Benny Iko <benny.iko.it@gmail.com>
2024-03-11 00:19:15 -0400

    On branch master: fixes bug soup.py 3.6.6

--- api/soup.py
+++ api/soup.py
@@ -1,5 +1,5 @@
 class soup:
-    def _init(self, filename):
+    def __init__(self, filename):
         self.soup_obj = get_soup(filename)
     
     #this function returns the Beautiful Soup object.
