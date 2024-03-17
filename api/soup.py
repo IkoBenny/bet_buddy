d3b0a7897dda0fbdb3ae99ccec617500fdde8c08
Benny Iko <benny.iko.it@gmail.com>
2024-03-17 16:04:27 -0400

    On branch master: fixes minor bug soup.py 3.6.8 Changes to be committed: 	modified:   soup.py 	adds self keyword to getter -> get_soup() soup.py 3.6.7.
    	source example:
    
    	suns_vs_jazz = soup("bet_buddy\\data\\2023\\Suns 140-137 Jazz (Nov 19, 2023) Final Score - ESPN.html")
    	get_all_player_names(suns_vs_jazz.soup_obj)
    
    	['K. Durant',
    	'J. Nurkic',
    	'E. Gordon',
    	'G. Allen',
    	'D. Booker',
    	'D. Eubanks',
    	'K. Bates-Diop',
    	'J. Okogie',
    	'N. Little',
    	'J. Goodwin',
    	'C. Metu',
    	'U. Azubuike',
    	'B. Bol',
    	'S. Lee',
    	'J. Collins',
    	'L. Markkanen',
    	'J. Clarkson',
    	'O. Agbaji',
    	'K. George',
    	'K. Olynyk',
    	'S. Fontecchio',
    	'C. Sexton',
    	'T. Horton-Tucker',
    	'L. Samanic',
    	'T. Hendricks',
    	'B. Sensabaugh',
    	'O. Yurtseven',
    	'K. Dunn']

--- api/soup.py
+++ api/soup.py
@@ -5,7 +5,7 @@ class soup:
         self.soup_obj = get_soup(filename)
     
     #this function returns the Beautiful Soup object.
-    def get_soup(filename):
+    def get_soup(self, filename):
         with open(filename) as fp:
             soup = BeautifulSoup(fp, "html.parser")
             return soup
