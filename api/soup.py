b96ddb30342fa070b9dd7f3d3b925292771d0af0
Benny Iko <benny.iko.it@gmail.com>
2024-03-17 21:50:52 -0400

    On branch master: adds filename field soup.py 3.6.10
    Changes to be committed:
    	modified:   soup.py
    	adds filename field soup.py 3.6.10

--- api/soup.py
+++ api/soup.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 
 class soup:
     def __init__(self, filename):
+        self.filename = filename
         self.soup_obj = get_soup(filename)
     
     #this function returns the Beautiful Soup object.
