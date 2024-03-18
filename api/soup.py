d046e950b997eb597da15fb22f66aeefea2aed16
Benny Iko <benny.iko.it@gmail.com>
2024-03-17 21:08:41 -0400

    On branch master: adds typing to get_soup() soup.py 3.6.9 Changes to be committed: 	modified:   soup.py
    	typing — Support for type hints¶
    	https://docs.python.org/3/library/typing.html
    
    	Example:
    	The function below takes and returns a string and is annotated as follows:
    
    	def greeting(name: str) -> str:
        return 'Hello ' + name

--- api/soup.py
+++ api/soup.py
@@ -5,7 +5,7 @@ class soup:
         self.soup_obj = get_soup(filename)
     
     #this function returns the Beautiful Soup object.
-    def get_soup(self, filename):
+    def get_soup(self, filename) -> BeautifulSoup:
         with open(filename) as fp:
             soup = BeautifulSoup(fp, "html.parser")
             return soup
